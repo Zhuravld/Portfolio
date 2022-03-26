d7c3f5bb1ab0258a1d320cf35c9d3aac5d730c64
David Zhuravlev <zhuravld@gmail.com>
2022-03-27 00:19:53 +0100

    update demo using new pieces

--- TetrisOR/main.py
+++ TetrisOR/main.py
@@ -1,6 +1,12 @@
 from game import Game
+from pieces import *
 from solvers.heuristic import solve_brute_force
 
 if __name__ == "__main__":
-    G = Game(grid_shape=(8, 12), pieces={"CY": 32})
-    solve_brute_force(G, verbose=False)
+
+    G = Game(grid_shape=(5,3), pieces={
+        piece.code: 1
+        for piece in [MintGreen, EmeraldGreen, Yellow]
+    })
+
+    solve_brute_force(G, verbose=True)
